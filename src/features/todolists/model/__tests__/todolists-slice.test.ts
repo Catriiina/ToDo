06396c71d6afa0a215d13174e1cc1c637d15bc85
Catriiina <catherinaignatovich@gmail.com>
2025-12-09 14:41:13 +0100

    refactoring test

--- src/features/todolists/model/__tests__/todolists-slice.test.ts
+++ src/features/todolists/model/__tests__/todolists-slice.test.ts
@@ -17,8 +17,8 @@ beforeEach(() => {
   todolistId2 = nanoid()
 
   startState = [
-    { id: todolistId1, title: 'What to learn', addedDate: '', order: 0, filter: 'all' },
-    { id: todolistId2, title: 'What to buy', addedDate: '', order: 0, filter: 'all' },
+    { id: todolistId1, title: 'What to learn', addedDate: '', order: 0, filter: 'all', entityStatus: "idle" },
+    { id: todolistId2, title: 'What to buy', addedDate: '', order: 0, filter: 'all', entityStatus: "loading"},
   ]
 })
 
@@ -45,6 +45,7 @@ test("correct todolist should be created", () => {
     addedDate: '',
     order: 0,
     filter: 'all',
+    entityStatus: "idle"
   }
   const endState = todolistsReducer(
       startState,
